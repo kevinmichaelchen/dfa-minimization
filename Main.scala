c79a80cde5e21fe378c646278b7fb7f8e72a40f2
kevinchen93 <meejathoughts@gmail.com>
2014-10-05 15:47:27 -0400

    not importing properly

--- Main.scala
+++ Main.scala
@@ -8,27 +8,34 @@ object Main {
       println("Thou shalt provide the name of a file containing an adjacency matrix describing the DFA.")
       println("""
 For example:
-3,4
 _,0,1,_,_,_
 0,_,_,1,_,_
-_,_,_,_,0,1
-_,_,_,_,0,1
+_,_,_,_,0,1*
+_,_,_,_,0,1*
 _,_,_,_,0,1
 _,_,_,_,_,01
 """)
-      println("The first line of the file indicates the accept states. In this case, state/row 3 and state/row 4. States/rows are 1-indexed.")
       println("There are 6 states as indicated by the number of rows (or columns)")
-      println("The first state/row of the matrix is the start state (state 1)")
+      println("States are 1-indexed.")
+      println("State 1 (the first row) is the start state.")
+      println("Accept state/rows end with an asterisk.")
       println("When multiple symbols lead to the same state, we simply concatenate them (which isn't a problem since symbols are 1 char long anyway).")
       println("Row 1 indicates all the states that State 1 can get to. If a state cannot get to another state, we use an underscore.")
 
       System.exit(1)
     }
 
+    
+    val states: Set[State] = Set()
+
     val lines = Source.fromFile(args(0)).getLines.toArray
     for (i <- 0 until lines.length) {
       println(lines(i))
+      states += State(Symbol(i+""))
     }
+
+    println("states...")
+    println(states.toString)
     
   }
 
