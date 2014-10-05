a7221c83680e041a53b86a29c4e7adc639701eb8
kevinchen93 <meejathoughts@gmail.com>
2014-10-05 15:27:45 -0400

    added sample DFA

--- Main.scala
+++ Main.scala
@@ -3,7 +3,30 @@ import scala.io.Source
 object Main {
 
   def main(args: Array[String]) {
-    println(args.length)
+
+    if (args.length != 1) {
+      println("Thou shalt provide the name of a file containing an adjacency matrix describing the DFA.")
+      println("""
+For example:
+3,4
+_,0,1,_,_,_
+0,_,_,1,_,_
+_,_,_,_,0,1
+_,_,_,_,0,1
+_,_,_,_,0,1
+_,_,_,_,_,01
+""")
+      println("The first line of the file indicates the accept states. In this case, state/row 3 and state/row 4. States/rows are 1-indexed.")
+      println("There are 6 states as indicated by the number of rows (or columns)")
+      println("The first state/row of the matrix is the start state (state 1)")
+      println("When multiple symbols lead to the same state, we simply concatenate them (which isn't a problem since symbols are 1 char long anyway).")
+      println("Row 1 indicates all the states that State 1 can get to. If a state cannot get to another state, we use an underscore.")
+    }
+
+    println("Printing states")
+    for (line <- Source.fromFile(args(0)).getLines)
+      print(line)
+
     
   }
 
